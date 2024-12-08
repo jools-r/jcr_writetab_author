29bd7c1f51a613ff284047309a03977249d27d46
jools-r <julian@txpbuilders.com>
2024-12-08 18:31:58 +0100

    Initial commit
    jcr_writetab_author plugin files

---
+++ README.md
@@ -0,0 +1,23 @@
+# jcr_writetab_author
+
+Optionally assign an article to another author from within the @Write@ panel.
+
+---
+
+The plugin adds an author select dropdown to the `Write` panel after the “Sort and Display” block.
+
+## Installation
+
+Download and copy the plugin code to the plugin installer textarea. Install and verify to begin the automatic setup. After activating the plugin, you will see an Author select dropdown in a new “Author” subsection of the sidebar in the Write panel.
+
+## Troubleshooting
+
+Q: I don’t see an author select dropdown
+
+A: For the author select dropdown to appear, your site must have more than one author.
+
+## Changelog
+
+### 0.1.0 - 2024-12-08
+
+- Initial public release.
---
+++ jcr_writetab_author.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * jcr_writetab_author
+ *
+ * A Textpattern CMS plugin for setting the article author on the Write panel.
+ */
+
+global $event;
+
+if (txpinterface === 'admin') {
+    if ($event === 'article') {
+        new jcr_writetab_author();
+    }
+}
+
+class jcr_writetab_author
+{
+    /**
+     * Constructor.
+     */
+    public function __construct()
+    {
+        // Add article author UI region after sort_display block
+        register_callback(function($event, $step, $default, $rs) {
+            return $this->article_author_select($default, $rs);
+        }, 'article_ui', 'sort_display', 0);
+
+        // Amend author on article post/save
+        register_callback(array($this, 'article_author_amend'), 'article_posted');
+        register_callback(array($this, 'article_author_amend'), 'article_saved');
+    }
+
+    /**
+     * Render article author select UI
+     *
+     * @param string  $default standard html output
+     * @param array   $rs  article data
+     * @return string HTML
+     */
+
+     protected function article_author_select($default, $rs)
+    {
+        $out="";
+        $author = $rs['AuthorID'];
+        $all_authors = array();
+
+        foreach (safe_rows("name, RealName", 'txp_users', "1=1 ORDER BY RealName ASC") as $user) {
+            extract($user);
+            $all_authors[$name] = $RealName;
+        }
+
+        // Only output author select if the site has multiple authors
+        if (count($all_authors) > 1) {
+            $out = wrapRegion('txp-author-group',
+                inputLabel(
+                    'authorID',
+                    selectInput('AuthorID', $all_authors, $author, false),
+                    'author',
+                    array('', 'instructions_authorID'),
+                    array('class' => 'txp-form-field authorID')
+                ),
+                'txp-author-group-content',
+                'author',
+                'author'
+            );
+        }
+
+        // Append output after sort_display block
+        return $default.$out;
+    }
+
+    /**
+     * Update article author on article post/save.
+     *
+     * @param string $event
+     * @param string $step
+     * @param array  $rs
+     */
+
+    public function article_author_amend($event, $step, $rs)
+    {
+        // Update author on save
+        safe_update('textpattern', "AuthorID='".doSlash($rs['AuthorID'])."'", 'ID='.intval($rs['ID']));
+    }
+
+}
